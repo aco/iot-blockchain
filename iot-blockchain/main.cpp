65cd18e058bccc7e64078578e74626f0607f803c
aco <22728373+aco@users.noreply.github.com>
2019-06-13 21:38:31 +0100

    Initial Commit

---
+++ iot-blockchain/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  iot-blockchain
+//
+//  Created by aco on 13/06/2019.
+//  Copyright © 2019 aco. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
